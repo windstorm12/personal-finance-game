ebda49645a47f2e1e966d71f7f524ac6e71787b4
windstorm12 <115856461+windstorm12@users.noreply.github.com>
2025-07-22 16:42:43 -0700

    Exporting and downloading data

---
+++ download_csv_server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = process.env.PORT || 3000;
+
+app.get('/download', (req, res) => {
+  const filePath = path.join(__dirname, 'game_progress.csv');
+  res.download(filePath, (err) => {
+    if (err) {
+      res.status(404).send('File not found or error downloading.');
+    }
+  });
+});
+
+app.listen(port, () => {
+  console.log(`CSV download server running on port ${port}`);
+  console.log(`Visit http://localhost:${port}/download to download the CSV file.`);
+}); 
